d30c893e1f551a1a594c0ef83e6639d166917874
tianyong90 <412039588@qq.com>
2018-08-28 03:21:54 +0800

    remove unused packages

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,21 @@
 import vue from 'rollup-plugin-vue'
 import resolve from 'rollup-plugin-node-resolve'
 import css from 'rollup-plugin-css-only'
+import path from 'path'
+import fs from 'fs'
+
+const components = require('./get-components')()
+
+let externals = components.map(component => {
+  if (fs.existsSync(path.resolve(`./packages/${component}/index.vue`))) {
+
+    return path.resolve(`./packages/${component}/index.vue`)
+  } else if (fs.existsSync(path.resolve(`./packages/${component}/index.js`))) {
+    return path.resolve(`./packages/${component}/index.js`)
+  }
+})
+
+console.log(externals)
 
 export default {
   input: 'packages/index.js',
@@ -8,7 +23,10 @@ export default {
     format: 'esm',
     file: 'haha.js'
   },
-  external: 'vue',
+  external: [
+    'vue',
+    ...externals
+  ],
   plugins: [
     css(),
     vue({
